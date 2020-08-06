b1e587d596aa55f0d2118fedf6584d45cbc7d5e3
andrew <cairnsy17@hotmail.co.uk>
2020-08-07 00:18:36 +0100

    welcome blocks, metrics, new icons, menu links

---
+++ src/app/components/info-block/info-block.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info-block',
+  templateUrl: './info-block.component.html',
+  styleUrls: ['./info-block.component.scss']
+})
+export class InfoBlockComponent implements OnInit {
+  @Input() icon:string;
+  @Input() value:string;
+  @Input() subtitle:string;
+  @Input() iconColorClass:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
